06fe3948ceb0e387fc3c70fd8876914d30a6ed0d
Fábio Souza <fabiobarboza7@hotmail.com>
2020-03-24 02:54:48 -0300

    api url

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://ps-project-back.herokuapp.com';
-export const API_URL = 'http://localhost:3000';
+export const API_URL = 'https://ps-project-back.herokuapp.com';
+// export const API_URL = 'http://localhost:3000';
